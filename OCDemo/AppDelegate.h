5ddee79f591e0b174fc9a2cd14b9d786db53fca3
Lonmee <Lonmee@126.com>
2021-04-07 14:09:49 +0800

    Initial Commit

---
+++ OCDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OCDemo
+//
+//  Created by Lonmee on 4/7/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OCDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  OCDemo
+//
+//  Created by Lonmee on 4/7/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
